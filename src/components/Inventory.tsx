1039020778ecbafb4e8ed19554aa15cb972bee97
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2024-04-20 01:25:46 -0700

    Addition: On the Inventory component, we added a modal ui to capture item details.

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 // Redux Imports
@@ -14,19 +14,88 @@ const StartPage: React.FC = () => {
     const dispatch = useDispatch();
     const { user, error, isLoading } = useUser();
 
+    // Used to control visibility of modal
+    const [showAddModal, setShowAddModal] = useState(false);
+
+    // Controls the item's different variables
+    const [itemName, setItemName] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [price, setPrice] = useState("");
+
+
     // Redux
-    const page = useSelector((state: any) => state.page.page)
+    const page = useSelector((state: any) => state.page.page);
     const userData = useSelector((state: any) => state.user.userData);
 
+    const handleAddButton = () => {
+        setShowAddModal(true)
+    }
 
+    const handleAddItem = () => {
 
-
+    }
 
 
 
     return (
-        <div className='w-[100vw] h-[100vh] bg-blue-500 flex justify-center items-center flex-col'>
-            <button className='w-[100px] h-[100px] bg-green-400 rounded-md flex justify-center items-center flex-col'>+</button>
+        <div className='flex flex-col p-2'>
+            {(showAddModal) && (
+                <form onSubmit={handleAddItem}>
+
+                    <div className='fixed top-0 left-0 w-full h-full bg-opacity-50 bg-black flex justify-center items-center z-40'>
+                        <div className='w-[900px] h-[500px] p-5 rounded-xl' style={{ backgroundColor: '#E6E6FA' }}>
+                            <button className="ml-[97%] font-extrabold text-xl" onClick={() => {
+                                setShowAddModal(false);
+                            }}>
+                                x
+                            </button>
+                            <div className='flex justify-between'>
+                                <div>
+                                    Placeholder
+                                    <button type="submit" className='font-extrabold bg-yellow-200 p-4 min-w-[150px] rounded-2xl'>Upload Image</button>
+
+                                </div>
+                                <div>
+                                    <div className='font-extrabold'>
+                                        Item Name:
+                                        <input
+                                            type="text"
+                                            placeholder='Item'
+                                            className='p-2 my-2 rounded-2xl shadow-xl border w-[100%] font-extrabold'
+                                            value={itemName}
+                                            onChange={(e) => setItemName(e.target.value)}
+                                        />
+                                    </div>
+                                    <div className='font-extrabold'>
+                                        Quantity:
+                                        <input
+                                            type="text"
+                                            placeholder='Amount in this budget'
+                                            className='p-2 my-2 rounded-2xl shadow-xl border w-[100%] font-extrabold'
+                                            value={quantity}
+                                            onChange={(e) => setQuantity(e.target.value)}
+                                        />
+                                    </div>
+                                    <div className='font-extrabold'>
+                                        Price:
+                                        <input
+                                            type="text"
+                                            placeholder='Amount in this budget'
+                                            className='p-2 my-2 rounded-2xl shadow-xl border w-[100%] font-extrabold'
+                                            value={price}
+                                            onChange={(e) => setPrice(e.target.value)}
+                                        />
+                                    </div>
+                                    <button type="submit" className='font-extrabold bg-yellow-200 p-4 min-w-[150px] rounded-2xl'>Submit Changes</button>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            )}
+            <button className='w-[100px] h-[100px] bg-green-400 rounded-md flex justify-center items-center flex-col' onClick={handleAddButton}>+</button>
         </div>
     );
 }
