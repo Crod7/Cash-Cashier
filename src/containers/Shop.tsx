f80e178dbedb93b148d5fdca49ffaa4d2abb65d2
Carlos Rodriguez <106417568+Crod7@users.noreply.github.com>
2024-04-22 20:05:59 -0700

    Fix: Corrected deployment.

--- src/containers/Shop.tsx
+++ src/containers/Shop.tsx
@@ -3,16 +3,11 @@ import Inventory from "../components/Inventory";
 
 const Shop: React.FC = () => {
 
-
-
     return (
         <div>
             <Inventory />
         </div>
     )
-
-
-
 }
 
 export default Shop;
